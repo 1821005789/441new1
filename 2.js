592619ef9eb4bbe39fb8e486b7ff9a4da9220a1e
1821005789 <103395745+1821005789@users.noreply.github.com>
2022-05-16 16:44:52 +0800

    Add files via upload

---
+++ 2.js
@@ -0,0 +1,23 @@
+function myFunction1() {
+    location.replace("http://product.m.dangdang.com/11023808146.html")
+  }
+
+function myFunction2() {
+    location.replace("https://ishare.iask.sina.com.cn/theme/RkdknWCenL.html")
+  }  
+
+function myFunction3() {
+    location.replace("https://wenku.baidu.com/view/0130afec1ae8b8f67c1cfad6195f312b3169ebec.html")
+  }
+
+function myFunction4() {
+    location.replace("https://item.jd.com/10041843647785.html")
+  }
+
+function myFunction5() {
+    location.replace("https://item.jd.com/10051026227567.html")
+  }  
+
+function myFunction6() {
+    location.replace("https://www.igo.cn/kpzt/gre_gmat/GREGMAT-1/?source=baidu&tm_medium=NB-WY&utm_term=nb-gre+gmat-PC&utm_content=gre-bxjg&tm_compain=buxiban%7Bgre%7D&e_keywordid=352188740471&e_keywordid2=257583567564&renqun_youhua=3180666&bd_vid=11131247337697276843")
+  }
